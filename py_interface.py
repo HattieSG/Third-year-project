d1a3bdfbdefee8cc92a4b6d9323551233f55a258
Harriet Salat Groves <hattiesg@hotmail.co.uk>
2021-03-27 16:11:56 +0000

    Update the python interface to recieve input from two sensors simultaneously

--- py_interface.py
+++ py_interface.py
@@ -1,4 +1,4 @@
-import serial
+import serial, sys
 import numpy as np
 
 if __name__ == "__main__":
@@ -6,37 +6,55 @@ if __name__ == "__main__":
 
     # Attempt to connect to the port with the arduino & sensors
     try:
+        print("Connecting to board 1...")
         ser = serial.Serial('COM5', 57600)
-        ser2 = serial.Serial('COM7', 57600)
+        # print("Connecting to board 2...")
+        # ser2 = serial.Serial('COM6', 57600)
     # If there is a failure to connect end the program
     except:
         print("Failed to connect")
-        exit()
+        sys.exit()
 
     # Run the code until ctrl+c is pressed then close the connection
     try:
-        signal = np.asarray([255]*10)
+        # Initialise lists to store the last 10 values from each of the sensors
+        sensor1 = np.asarray([255]*10)
+        sensor2 = np.asarray([255]*10)
+        
         # Run an infinite loop
         while True:
             # Iterate a counter in cycles of 10 to store output values for the last second
             for i in range(0,10):
                 # Read the raw data from the sensor
                 hello = ser.readline()
-                # Decode the data and exclude the last character (\n) and store in signal
-                signal[i] = float(hello[0:len(hello)-1].decode("utf-8"))   
-                print(signal)
-
-                # If the average signal value is low, send red led signal
-                if (np.mean(signal) < 150):
-                    ser2.write(str(1).encode())
-                # If the average signal value is medium, send yellow led signal
-                elif (np.mean(signal) < 220):
-                    ser2.write(str(2).encode())
-                # Otherwise the average signal value is high, send green led signal
+                # Decode the message
+                decoded = (hello.decode("utf-8"))
+                # Split the message by underscoreses to extract the reading from each sensor
+                sensor_readings = decoded[:-1].split("_") # Remove the last character ("\n")
+                # Append the sensor readings to the list for each sensor
+                sensor1[i] = float(sensor_readings[0])
+                sensor2[i] = float(sensor_readings[1])
+                #print(f"Sensor1: {sensor1}")
+                #print(f"Sensor2: {sensor2}")
+                
+                # If either sensor is experiencing pressure print to the console!
+                if (np.mean(sensor1) > 265 and np.mean(sensor2) > 265):
+                    print("Both sensors are on!")
+                elif (np.mean(sensor1) > 265):
+                    print("Sensor1 on!")
+                elif (np.mean(sensor2) > 265):
+                    print("Sensor2 on!")
                 else:
-                    ser2.write(str(3).encode())
+                    print("...")
+                    
+                    #ser2.write(str(2).encode())
+                    #ser2.write(str(1).encode())
+                    #ser2.write(str(3).encode())
     # When ctrl+c is pressed close the port
     except KeyboardInterrupt:
         print ('Caught KeyboardInterrupt')
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+    finally:
         ser.close()
-        ser2.close()
+        # ser2.close()
