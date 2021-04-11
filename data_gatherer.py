bd7459ce87262a157ce212a0a4b4fc4113e52fef
Harriet Salat Groves <hattiesg@hotmail.co.uk>
2021-04-11 14:59:36 +0100

    Complete data gathering script to record sensor data to a .csv file labelled with 0 or 1 for grasp fail or grasp success

--- data_gatherer.py
+++ data_gatherer.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Global variable, thread running
 thread_running = True
-user_input = "1"
+user_input = ""
 
 
 def take_input():
@@ -13,14 +13,28 @@ def take_input():
     global user_input
     while thread_running == True:
         user_input = input('Type user input: ')
-        
+         
     
 if __name__ == "__main__":
     print("Hello world")
+    # Attempt to connect to the port with the arduino & sensors
+    try:
+        print("Connecting to board 1...")
+        ser = serial.Serial('COM7', 57600)
+    # If there is a failure to connect end the program
+    except:
+        print("Failed to connect")
+        sys.exit()
+        
     # Read csv of sensor data
     try:
+        print("Connected!")
         print("Reading data...")
         df = pd.read_csv("pinch_data.csv")
+        for col in df.columns:
+            if "Unnamed" in col:
+                df=df.drop(col, axis=1)
+    # If no csv exists create a new empty csv
     except: 
         print("Could not ready csv file... creating new one...")
         df = pd.DataFrame(columns=["S1_mean","S1_t1", "S1_t2", "S1_t3", "S1_t4",
@@ -31,31 +45,66 @@ if __name__ == "__main__":
                                    "S2_t10", "Label", "Timestamp"])
         df.to_csv("pinch_data.csv", index=False)
         print("File created.")
+    
     try:
-        u_in = input("To record pinch data press 1, to record \'not pinch\' data press 0: ")
-        if (u_in == "1"):
-            print("Recording pinches, press enter to save or q to quit...")
+        print("Data loaded!")
 
-            # Create threads to recieve user input
-            input_thread = Thread(target=take_input)
-            # Start the thread
-            input_thread.start()
-            # Print the variable infinitely
-            
-            time.sleep(1)
-            
-            ctr = 0
-            while True:
-                ctr += 0.001
-                if user_input == "":
-                    print(ctr)
-                    user_input = "1"
-
-            
+        # Create threads to recieve user input
+        input_thread = Thread(target=take_input)
+        # Start the thread
+        input_thread.start()
+        time.sleep(1)
+        
+        
+        # Initialise lists to store the last 10 values from each of the sensors
+        sensors = np.asarray([np.asarray([255]*10)]*2)
+        
+        while True:
+            # Iterate a counter in cycles of 10 to store output values for the last second
+            for i in range(0,10):
+                # Read the raw data from the sensor
+                hello = ser.readline()
+                # Decode the message
+                decoded = (hello.decode("utf-8"))
+                # Split the message by underscoreses to extract the reading from each sensor
+                sensor_readings = decoded[:-1].split("_") # Remove the last character ("\n")
+                # Iterate through all sensor readings and save
+                for j in range(0,len(sensor_readings)):
+                    sensors[j,i] = sensor_readings[j]
+                
+                # If the user input is one save the data as a grasp
+                if user_input == "1":
+                    print("Grasp!")
+                    # Create the row of values for the csv
+                    row = ([np.mean(sensors[0,:])] + sensors[0,:].tolist() + 
+                           [np.mean(sensors[1,:])] + sensors[1,:].tolist() + 
+                           [user_input] + [str(datetime.datetime.now())])
+                    print(row)
+                    # Append the values to the csv
+                    df = df.append(pd.Series(row, index = df.columns),
+                                   ignore_index=True)
+                    user_input = ""
+                
+                # 
+                if user_input =="0":
+                    print("Fail grasp!")
+                    row = ([np.mean(sensors[0,:])] + sensors[0,:].tolist() + 
+                           [np.mean(sensors[1,:])] + sensors[1,:].tolist() + 
+                           [user_input] + [str(datetime.datetime.now())])
+                    print(row)
+                    df = df.append(pd.Series(row, index = df.columns),
+                                   ignore_index=True)
+                    user_input = ""
+                    
     except KeyboardInterrupt:
         print ('Caught KeyboardInterrupt')
+        
     except:
         print("Unexpected error:", sys.exc_info()[0])
     finally:
-        sys.exit()
         print("Program ended.")
+        print("Please restart the terminal.")
+        df.to_csv("pinch_data.csv")
+        ser.close()
+        sys.exit()
+        
