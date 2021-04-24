789b9d850b8178f39e6427a9614efaff0104d38a
Harriet Salat Groves <hattiesg@hotmail.co.uk>
2021-04-24 15:30:32 +0100

    Modify the python interface to load a model and send a command to the hand based on the predicted grasp state

--- py_interface.py
+++ py_interface.py
@@ -1,4 +1,7 @@
-import serial, sys
+import pickle
+import serial
+import sklearn
+import sys
 import numpy as np
 
 if __name__ == "__main__":
@@ -7,47 +10,50 @@ if __name__ == "__main__":
     # Attempt to connect to the port with the arduino & sensors
     try:
         print("Connecting to board 1...")
-        ser = serial.Serial('COM5', 57600)
+        ser = serial.Serial('COM7', 57600)
         print("Connecting to board 2...")
-        ser2 = serial.Serial('COM6', 57600)
-        
+        ser2 = serial.Serial('COM8', 9600)        
     # If there is a failure to connect end the program
     except:
         print("Failed to connect")
         sys.exit()
+        
+    # Load the machine learning model of choice
+    try:
+        print("Loading machine learning model...")
+        model = pickle.load(open("logistic_regression", 'rb'))
+        #model = pickle.load(open("svm", 'rb'))
+        #model = pickle.load(open("random_forest", 'rb'))
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        sys.exit()
+        
 
     # Run the code until ctrl+c is pressed then close the connection
     try:
-        print("Connected!")
         # Initialise lists to store the last 10 values from each of the sensors
-        sensors = np.asarray([np.asarray([255]*10)]*4)
+        sensors = np.asarray([np.asarray([255]*10)]*2)
         
         # Run an infinite loop
         while True:
-            # Iterate a counter in cycles of 10 to store output values for the last second
-            for i in range(0,10):
-                # Read the raw data from the sensor
-                hello = ser.readline()
-                # Decode the message
-                decoded = (hello.decode("utf-8"))
-                # Split the message by underscoreses to extract the reading from each sensor
-                sensor_readings = decoded[:-1].split("_") # Remove the last character ("\n")
-                # Iterate through all sensor readings
-                for j in range(0,len(sensor_readings)):
-                    sensors[j,i] = sensor_readings[j]
-                # Create a counter to count the number of sensors that are on
-                on_ctr = 0
-                # Iterate throug each sensor
-                for i in range(0,4):
-                    # If the average for the past second of readings is great than 290, count sensor as on
-                    if np.mean(sensors[i,:]) > 275:
-                        # Iterate count of on sensors
-                        on_ctr+=1
-                # Write to the arduino the number of sensors under pressure
-                ser2.write(str(on_ctr).encode())
-                print(on_ctr)
-                print(ser2.readline())
-                
+            # Read the raw data from the sensor
+            hello = ser.readline()
+            # Decode the message
+            decoded = (hello.decode("utf-8"))
+            # Split the message by underscoreses to extract the reading from each sensor
+            sensor_readings = decoded[:-1].split("_") # Remove the last character ("\n")
+            # Iterate through all sensor readings recieved and save to appropriate array
+            for j in range(0,len(sensor_readings)):
+                # Append the current value to the appropriate array, and remove the oldest value
+                sensors[j] = np.append(np.delete(sensors[j], 0), sensor_readings[j])
+            
+            # Create a array of sensor values to pass into the model
+            X = np.concatenate((sensors[0,:], sensors[1,:]), axis=None)
+            # Query the model using the sensor values
+            pred = model.predict(X.reshape(1,-1)) # Reshape array as only one sample
+            # Write the model output to the board            
+            ser2.write(str(pred[0]).encode())
+            
     # When ctrl+c is pressed close the port
     except KeyboardInterrupt:
         print ('Caught KeyboardInterrupt')
